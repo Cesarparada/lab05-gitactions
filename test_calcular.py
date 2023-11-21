f4fabf941b43c726571383cc1d69dac8197d60f7
César David Parada Castillo <cesard.0925@gmail.com>
2023-11-21 19:01:43 +0100

    feat: agregadar las operaciones y los test  en  un solo fichero

---
+++ test_calcular.py
@@ -0,0 +1,23 @@
+import unittest
+from calcular import sumar, restar, multiplicar
+
+class TestCalcular(unittest.TestCase):
+ 
+     def test_sumar(self):
+            self.assertEqual(sumar(3, 2), 5)
+            self.assertEqual(sumar(-1, 1), 0)
+            self.assertEqual(sumar(-1, -1), -2)
+
+
+     def test_restar(self):
+            self.assertEqual(restar(5, 2), 3)
+            self.assertEqual(restar(-1, 1), -2)
+            self.assertEqual(restar(-1, -1), 0)
+
+     def test_multiplicar(self):
+            self.assertEqual(multiplicar(5, 2), 10)
+            self.assertEqual(multiplicar(-1, 1), -1)
+            self.assertEqual(multiplicar(-1, -1), 1)
+
+if __name__ == '__main__':
+        unittest.main()
